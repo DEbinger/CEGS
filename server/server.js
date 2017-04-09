56a19bce9d7478eeac5becd06228346446b213e0
nms377 <nms377@nyu.edu>
2017-04-08 20:21:07 -1000

    Create an .env file
    - setup .env file in the server directory
    - stored api key for hotel and car
    
    resolves #34

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = process.env.PORT || 9000;
 const bodyParser = require('body-parser');
 
+const {HOTEL_CAR_KEY, FLIGHT_KEY} = process.env;
+
 app.use(bodyParser.urlencoded( {
   extended : true
 }));
