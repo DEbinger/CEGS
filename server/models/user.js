1680e5417414a3859a21cda1fedc5bee2b455d3a
nms377 <nms377@nyu.edu>
2017-04-10 19:21:59 -1000

    Removed Users to the classMethods object because Users can't relate to itself.

--- server/models/user.js
+++ server/models/user.js
@@ -9,7 +9,9 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        User.hasMany(models.Flight);
+        User.hasMany(models.Hotel);
+        User.hasMany(models.Car);
       }
     }
   });
