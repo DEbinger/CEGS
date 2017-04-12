9b8e895a7628128026b4fa49a5155e0a3bdf604d
David <davidebinger888@gmail.com>
2017-04-11 17:10:58 -1000

    added .gitignore to ignore auth.js file with secret keys and id, added auth sample in its place. Added auth/passport.js for auth and made additions to server.js that includes passport

---
+++ auth/auth.sample.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'facebookAuth' : {
+    'clientID': 'xxxxxxxxxxxx',
+    'clientSecret' : 'xxxxxxxxxxxx',
+    'callbackURL' : 'http://localhost:9000/auth/facebook/callback'
+  },
+  'googleAuth' : {
+    'clientID': 'xxxxxxxxxxxx',
+    'clientSecret' : 'xxxxxxxxxxxx',
+    'callbackURL' : 'http://localhost:9000/auth/google/callback'
+  }
+};
--- server/server.js
+++ server/server.js
@@ -1,15 +1,38 @@
+// jshint esversion:6
+
 const express = require('express');
+const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 9000;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const passport = require('passport');
+const flash = require('connect-flash');
+const LocalStrategy = require('passport-local').Strategy;
 
+require('../auth/passport')(passport);
+
+app.use(cookieParser());
 app.use(bodyParser.urlencoded( {
   extended : true
 }));
 
+app.use(session({
+  secret: 'anystringoftext',
+  saveUninitialized: true,
+  resave: true
+  }));
+
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
 app.use('/', (req, res) => {
   res.send('testing');
 });
+
+require('../routes/routes.js')(app, passport);
 
 app.listen(PORT, () => {
   console.log("Server listening on", PORT);
