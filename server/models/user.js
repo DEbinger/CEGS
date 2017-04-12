30a2bb94ac6e596c2fc5ce1da73d9d85cce2752d
nms377 <nms377@nyu.edu>
2017-04-11 20:13:24 -1000

    Added Cars model to assocations in classMethod.

--- server/models/user.js
+++ server/models/user.js
@@ -11,6 +11,7 @@ module.exports = function(sequelize, DataTypes) {
       associate: function(models) {
         User.hasMany(models.Flight);
         User.hasMany(models.Hotel);
+        User.hasMany(models.Car);
       }
     }
   });
