71774eb721dfa5c5a22d592bf0ec2a8ef10a284c
nms377 <nms377@nyu.edu>
2017-04-13 19:51:30 -1000

    Setup the sign in form. Just put email and password for now.

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 class SignIn extends Component {
   render() {
     return (
-      <h1>SIGN IN PAGE</h1>
+      <div>
+        <h1>SIGN IN</h1>
+
+        <form>
+          <input type='email' placeholder='Email Address' autocomplete='off' />
+          <br />
+          <input type='password' placeholder='Password' autocomplete='off' />
+          <br />
+          <input type='submit' value='Login' />
+        </form>
+      </div>
     );
   }
 }
