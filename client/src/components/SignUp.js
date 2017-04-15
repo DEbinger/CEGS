32b3e829c49a10f99c7263fa1818612850c6bbcb
nms377 <nms377@nyu.edu>
2017-04-15 09:37:53 -1000

    Updated input placeholder values and included buttons for users to sign up with Google or Facebook.

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -11,13 +11,14 @@ class SignUp extends Component {
           <br />
           <input type='text' placeholder='Last Name' autocomplete='off' />
           <br />
-          <input type='email' placeholder='Email Address' autocomplete='off' />
+          <input type='email' placeholder='Email' autocomplete='off' />
           <br />
           <input type='password' placeholder='Password' autocomplete='off' />
           <br />
           <input type='password' placeholder='Confirm Password' autocomplete='off' />
           <br />
           <select name="security_question">
+            <option>Select a Security Question</option>
             <option value="color">What is your favorite color?</option>
             <option value="school">What is the name of the high school you attended?</option>
             <option value="birthplace">What city were you born in?</option>
@@ -27,6 +28,8 @@ class SignUp extends Component {
           <br />
           <input type='submit' value='Sign Up' />
         </form>
+        <input type='submit' value='Sign Up with Google' />
+        <input type='submit' value='Sign Up with Facebook' />
       </div>
     );
   }
