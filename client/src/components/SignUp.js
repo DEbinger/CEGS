e032d4580d950ff085e7a17f4bafd90f9c2b784e
nms377 <nms377@nyu.edu>
2017-04-15 14:56:07 -1000

    Set the initial state of the form values. Included the value of the input form values.

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,29 +1,44 @@
 import React, { Component } from 'react';
 
 class SignUp extends Component {
+
+constructor () {
+  super();
+
+    this.state = {
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+    security_question: '',
+    security_answer: ''
+  };
+
+}
+
   render() {
     return (
       <div>
         <h1>SIGN UP</h1>
         <form>
-          <input type='text' placeholder='First Name' autocomplete='off' />
+          <input type='text' name="first_name" value={this.state.first_name} placeholder='First Name' autoComplete='off' />
           <br />
-          <input type='text' placeholder='Last Name' autocomplete='off' />
+          <input type='text' name="last_name" value={this.state.last_name} placeholder='Last Name' autoComplete='off' />
           <br />
-          <input type='email' placeholder='Email' autocomplete='off' />
+          <input type='email' name="email" value={this.state.email} placeholder='Email' autoComplete='off' />
           <br />
-          <input type='password' placeholder='Password' autocomplete='off' />
+          <input type='password' name="password" value={this.state.password} placeholder='Password' autoComplete='off' />
           <br />
-          <input type='password' placeholder='Confirm Password' autocomplete='off' />
+          <input type='password' placeholder='Confirm Password' autoComplete='off' />
           <br />
-          <select name="security_question">
-            <option>Select a Security Question</option>
+          <select name="security_question" value={this.state.security_question}>
+            <option selected disabled>Select a Security Question</option>
             <option value="color">What is your favorite color?</option>
             <option value="school">What is the name of the high school you attended?</option>
             <option value="birthplace">What city were you born in?</option>
           </select>
           <br />
-          <input type='text' placeholder='Security Answer' autocomplete='off' />
+          <input type='text' name="security_answer" value={this.state.security_answer} placeholder='Security Answer' autoComplete='off' />
           <br />
           <input type='submit' value='Sign Up' />
         </form>
