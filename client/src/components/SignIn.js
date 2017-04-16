8690571e9fd5745966970d887a9c482a26626d3b
nms377 <nms377@nyu.edu>
2017-04-15 17:31:11 -1000

    Updated the autoComplete attribute in the form inputs. It was not written in camel case and the console was sending warnings.

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -2,15 +2,42 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class SignIn extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      first_name: '',
+      last_name: '',
+      email: '',
+      password: '',
+      security_question: '',
+      security_answer: ''
+    };
+  }
+
+
+  submitHandler(event) {
+    event.preventDefaul();
+    let oReq = new XMLHttpRequest();
+    oReq.addEventListener('load', (user) => {
+      let data = JSON.parse(user.target.responseText);
+      console.log(data);
+    });
+
+    oReq.open('GET', '/signup');
+    oReq.send();
+  }
+
   render() {
     return (
       <div>
         <h1>SIGN IN</h1>
 
         <form>
-          <input type='email' placeholder='Email Address' autocomplete='off' />
+          <input type='email' placeholder='Email Address' autoComplete='off' />
           <br />
-          <input type='password' placeholder='Password' autocomplete='off' />
+          <input type='password' placeholder='Password' autoComplete='off' />
           <br />
           <input type='submit' value='Login' />
           <br />
