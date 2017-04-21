86287dfb28e7dafa2833fa837ae4d9d6e3f02a2e
nms377 <nms377@nyu.edu>
2017-04-20 18:52:32 -1000

    Added id parameters to include with user data when they sign up and it gets stored to data.

--- client/src/redux/actions/usersAction.js
+++ client/src/redux/actions/usersAction.js
@@ -1,8 +1,9 @@
 export const ADD_USER = 'ADD_USER';
 
-export function addUser(first_name, last_name, email, password, confirm_password, security_question, security_answer) {
+export function addUser(id,first_name, last_name, email, password, confirm_password, security_question, security_answer) {
   return {
     type: ADD_USER,
+    id,
     first_name,
     last_name,
     email,
