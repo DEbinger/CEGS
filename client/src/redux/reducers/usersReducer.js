bef0da011970e3d44d7f5b8e8ba9eb80fd2f3bd9
nms377 <nms377@nyu.edu>
2017-04-20 18:54:02 -1000

    Trying to created an if statement that will check if the password and confirm password match in order to add it to the state and database. I can get action data but that data hashes the passwords and I need to compare the string passwords before they are hashed. Users returns the function but I cannot seem to find a way to access the data prior to it going through action.

--- client/src/redux/reducers/usersReducer.js
+++ client/src/redux/reducers/usersReducer.js
@@ -5,22 +5,29 @@ const initialState = {
 };
 
 function users(state = initialState, action) {
+  console.log("ACTION: ", action.password);
   switch(action.type) {
     case ADD_USER:
-      return Object.assign({}, state, {
-        users: [
-          ...state.users,
-          {
-            first_name: action.first_name,
-            last_name: action.last_name,
-            email: action.email,
-            password: action.password,
-            confirm_password: action.confirm_password,
-            security_question: action.security_question,
-            security_answer: action.security_answer
-          }
-        ]
-      });
+      if(users.password === users.confirm_password){  // does not work
+        console.log('SUCCESS', users);                // can get action
+        return Object.assign({}, state, {             // but hashes pws
+          users: [                                    // need data before hash
+            ...state.users,
+            {
+              id: action.id,
+              first_name: action.first_name,
+              last_name: action.last_name,
+              email: action.email,
+              password: action.password,
+              confirm_password: action.confirm_password,
+              security_question: action.security_question,
+              security_answer: action.security_answer
+            }
+          ]
+        });
+      } else {
+        console.log('Passwords don\'t match');
+      }
 
     default:
       return state;
