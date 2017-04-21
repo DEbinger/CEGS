ace0d70a4ffc726717ec427208f5425e78f9e390
nms377 <nms377@nyu.edu>
2017-04-20 21:49:59 -1000

    Commented out the functions and XHR requests that were not added to the lib direcotry or actions/useresAction.js file. Created a simple dummy component to render a header.

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux';
-import { addUserToState } from '../actions';
-import { Signin } from './Signin';
+// import { addUserToState } from '../actions';
+import { Signin } from './SignIn';
 
 import {
   BrowserRouter as Router,
@@ -10,63 +10,71 @@ import {
   Link
 } from 'react-router-dom';
 
-class secret extends Component {
+class Profile extends Component {
   constructor(props){
     super(props);
   }
 
-  xhrLoginCheck(){
-    return new Promise(function(resolve,reject){
-      function reqListener(){
-        resolve(this.responseText);
-      }
-      let oReq = new XMLHttpRequest();
-      oReq.open('POST', '/users/signin');
-      oReq.setRequestHeader('Content-type',
-        'application/json')
-      oReq.addEventListener("load", reqListener)
-      oReq.send()
-    })
-  }
+  // xhrLoginCheck(){
+  //   return new Promise(function(resolve,reject){
+  //     function reqListener(){
+  //       resolve(this.responseText);
+  //     }
+  //     let oReq = new XMLHttpRequest();
+  //     oReq.open('POST', '/users/signin');
+  //     oReq.setRequestHeader('Content-type',
+  //       'application/json')
+  //     oReq.addEventListener("load", reqListener)
+  //     oReq.send()
+  //   })
+  // }
 
-  componentDidMount(){
-    this.xhrLoginCheck()
-    .then((userData)=>{
-      console.log(this.props)
-      let user = JSON.parse(userData)
-      this.props.onAddUser(user.id, user.email)
-    })
-    .catch(function(err){
-      console.log("component will mount error",err)
-    })
-  }
+  // componentDidMount(){
+  //   this.xhrLoginCheck()
+  //   .then((userData)=>{
+  //     console.log(this.props)
+  //     let user = JSON.parse(userData)
+  //     this.props.onAddUser(user.id, user.email)
+  //   })
+  //   .catch(function(err){
+  //     console.log("component will mount error",err)
+  //   })
+  // }
 
   render(){
-    if(this.props.loggedInUser) {
-      return (
-        <div>
-          <h1>this Profile</h1>
-        </div>
-      )
-    }
+    // if(this.props.loggedInUser) {
+    //   return (
+    //     <div>
+    //       <h1>this Profile</h1>
+    //     </div>
+    //   )
+    // }
+    // return (
+    //   <Redirect to={{
+    //     pathname: '/signin'
+    //   }}/>
+    // )
+
     return (
-      <Redirect to={{
-        pathname: '/signin'
-      }}/>
+      <div>
+        <h1>USER PROFILE</h1>
+      </div>
     )
   }
 }
 
-const mapDispatchToProps = {
-  onAddUser: addUserToState
-}
+// const mapDispatchToProps = {
+//   onAddUser: addUserToState
+// }
 
-const mapStateToProps = (state) => {
-  console.log("STATE", state)
-  return {
-    loggedInUser: state.loggedInUser
-  }
-}
+// const mapStateToProps = (state) => {
+//   console.log("STATE", state)
+//   return {
+//     loggedInUser: state.loggedInUser
+//   }
+// }
 
-export default connect(
-  mapStateToProps, mapDispatchToProps)(secret)
+// export default connect(
+//   mapStateToProps, mapDispatchToProps)(secret)
+
+export default Profile;
