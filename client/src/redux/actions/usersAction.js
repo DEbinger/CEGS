6da58ea0ce5ddfb027c510d359b4bd090b04f3d7
nms377 <nms377@nyu.edu>
2017-04-22 09:54:17 -1000

    Jon helped me refactor the user action. Using the redux devtools we found that state as already an object so we removed the object and key value pair in the initial state and just set it to an empty array. Then in the users function, we removed all the action dot notations and simply returned an array with dot spread notation state and action.user. I added a user and the redux devtool showed the added user in the state. I reviewed the debugging process with Chaz and he recommended removing state from the return aray because state would return all the users and there is no situation where we would need to call all the users at once unless we created an admin and an admin need to see all its users.

--- client/src/redux/actions/usersAction.js
+++ client/src/redux/actions/usersAction.js
@@ -1,15 +1,9 @@
 export const ADD_USER = 'ADD_USER';
 
-export function addUser(id,first_name, last_name, email, password, confirm_password, security_question, security_answer) {
+export function addUser(user) {
+
   return {
     type: ADD_USER,
-    id,
-    first_name,
-    last_name,
-    email,
-    password,
-    confirm_password,
-    security_question,
-    security_answer
+    user
   };
 }
--- client/src/redux/reducers/usersReducer.js
+++ client/src/redux/reducers/usersReducer.js
@@ -1,33 +1,16 @@
 import { ADD_USER } from '../actions/usersAction';
 
-const initialState = {
-  users: []
-};
+const initialState = [];
 
 function users(state = initialState, action) {
-  console.log("ACTION: ", action.password);
+  console.log("REDUCER: ", state);
   switch(action.type) {
     case ADD_USER:
-      if(users.password === users.confirm_password){  // does not work
-        console.log('SUCCESS', users);                // can get action
-        return Object.assign({}, state, {             // but hashes pws
-          users: [                                    // need data before hash
-            ...state.users,
-            {
-              id: action.id,
-              first_name: action.first_name,
-              last_name: action.last_name,
-              email: action.email,
-              password: action.password,
-              confirm_password: action.confirm_password,
-              security_question: action.security_question,
-              security_answer: action.security_answer
-            }
-          ]
-        });
-      } else {
-        console.log('Passwords don\'t match');
-      }
+        return [
+              action.user
+          ];
+
+        // return [action.user];
 
     default:
       return state;
