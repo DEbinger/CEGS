28fd152c99460425fdc3700f78c067ca096d09be
nms377 <nms377@nyu.edu>
2017-04-29 10:40:33 -1000

    Created functions to handle data coming from the hotel and cars components. If the object for hotels or cars is empty the function will render a message that says no hotel or car was selected. If a hotel/car is selected it will render the hotel and car details.

--- client/src/components/Itinerary.js
+++ client/src/components/Itinerary.js
@@ -8,39 +8,61 @@ class Itinerary extends Component {
 		
 	}
 
+  hotel(){
+    if(Object.keys(this.props.hotels.hotelDetail).length !== 0){
+      let hotel = this.props.hotels.hotelDetail;
+      return <div>
+          <p>{hotel.name}</p>
+          <p>{hotel.address.line1}, {hotel.address.city}, {hotel.address.region} {hotel.address.postal_code}</p>
+          { hotel.contacts.map( ({ detail, type }) => <p>{ type }: { detail }</p> )}
+          <p>{hotel.rating}</p>
+          <p>{hotel.amenities}</p>
+          <p>{hotel.cost}</p>
+        </div>
+    } else {
+      return <p>No hotel selected</p>
+    }
+  }
+
+  car(){
+    if(Object.keys(this.props.cars.car_details).length !== 0){
+      let car = this.props.cars.car_details;
+
+      return <div>
+          <p>{car.company_name}</p>
+          <p>{car.airport}</p>
+          <p>{car.city}</p>
+          <p>{car.category}</p>
+          <p>{car.vehicle_type}</p>
+          <p>{car.transmission}</p>
+          <p>{car.amount}</p>
+          <p>{car.fuel}</p>
+        </div>
+    } else {
+      return <p>No car selected</p>
+    }
+  }
+
   render() {
   	console.log('HOTEL: ', this.props.hotels.hotelDetail);
     console.log('CAR: ', this.props.cars.car_details);
-  	let hotel = this.props.hotels.hotelDetail;
-    let car = this.props.cars.car_details;
     return (
     	<div>
       	<h1>ITINERARY PAGE</h1>
 
       		<div>
       			<h3>Flight</h3>
+            <p>No flight selected</p>
       		</div>
 
 	      	<div>
 	      		<h3>Hotel</h3>
-	      		<p>{hotel.name}</p>
-	      		<p>{hotel.address.line1}, {hotel.address.city}, {hotel.address.region} {hotel.address.postal_code}</p>
-	        	{ hotel.contacts.map( ({ detail, type }) => <p>{ type }: { detail }</p> )}
-						<p>{hotel.rating}</p>
-						<p>{hotel.amenities}</p>
-						<p>{hotel.cost}</p>
+            { this.hotel() }
 					</div>
 
 					<div>
 						<h3>Car</h3>
-            <p>{car.company_name}</p>
-            <p>{car.airport}</p>
-            <p>{car.city}</p>
-            <p>{car.category}</p>
-            <p>{car.vehicle_type}</p>
-            <p>{car.transmission}</p>
-            <p>{car.amount}</p>
-            <p>{car.fuel}</p>
+            { this.car() }
 					</div>
       </div>
     );
