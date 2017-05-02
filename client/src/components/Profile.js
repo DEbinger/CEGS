a7d98d16ba5ce7a4c4ac3c8df5288ceb64efb09f
David <davidebinger888@gmail.com>
2017-05-01 20:47:23 -1000

    photo upload on profile page

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -6,15 +6,50 @@ import { connect } from 'react-redux';
 // import { Link } from ‘react-router-dom’;
 import getUserReq from '../../lib/userReq';
 import { addUser } from '../redux/actions/usersAction';
-// import fetch from ‘node-fetch’;
+import Dropzone from 'react-dropzone';
+import request from 'superagent';
 
-class Profile extends Component {
+const CLOUDINARY_UPLOAD_PRESET = 'gyfqpzrn';
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/hele.io/upload';
+
+
+class Profile extends React.Component {
  constructor(props){
-   super(props);
+  super(props);
 
-   this.signOut = this.signOut.bind(this);
+  this.state = {
+    uploadedFileCloudinaryUrl: ''
+  };
+
+  this.signOut = this.signOut.bind(this);
  }
 
+onImageDrop(files) {
+    this.setState({
+      uploadedFile: files[0]
+    });
+
+    this.handleImageUpload(files[0]);
+  }
+
+handleImageUpload(file) {
+    let upload = request.post(CLOUDINARY_UPLOAD_URL)
+      .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+      .field('file', file)
+      .end(file);
+
+    upload.end((err, response) => {
+      if (err) {
+        console.error(err);
+      }
+
+      if (response.body.secure_url !== '') {
+        this.setState({
+          uploadedFileCloudinaryUrl: response.body.secure_url
+        });
+      }
+    });
+  }
  // addUser(user){
  //   getUserReq(user)
  //    .then( user => {
@@ -82,10 +117,27 @@ class Profile extends Component {
  render(){
    console.log('FROM PROFILE', this.props);
    return (
-    <div>
-     <h1>USER PROFILE</h1>
+    <form>
+    <h1>USER PROFILE</h1>
+      <div>
+        <div className="FileUpload">
+          <Dropzone
+            multiple={false}
+            accept="image/jpg,image/png"
+            onDrop={this.onImageDrop.bind(this)}>
+            <p>Drop an image or click to select a file to upload.</p>
+          </Dropzone>
+        </div>
 
-    </div>
+        <div>
+          {this.state.uploadedFileCloudinaryUrl === '' ? null :
+          <div>
+            <p>{this.state.uploadedFile.name}</p>
+            <img src={this.state.uploadedFileCloudinaryUrl} />
+          </div>}
+        </div>
+      </div>
+    </form>
    );
  }
 }
