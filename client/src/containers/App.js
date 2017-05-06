9a457234948ff3424d06cd064876cefb4fa92912
nms377 <nms377@nyu.edu>
2017-05-06 09:00:10 -1000

    Changed the size of the home page image to stretch across the window. Made the Plan Your Adventure Today button bigger.

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -16,13 +16,11 @@ class App extends Component {
   render() {
     return (
     	<div id="home">
-        <div id="homepage-pic">
-          <img src="assets/homepage-pic.jpg" alt="Homepage Pic" height="450" width="850" />
-        </div>
+        <img id="homepage-pic" src="assets/homepage-pic.jpg" alt="Homepage Pic" />
         <div id="description">
           <p>Aloha and welcome to Hele! Hele means "go" or "travel" in Hawaiian and this site will help you do just that. Pick a destination and easily search for flights, hotels, and rental cars. Your travel information will be conveniently stored in your personal itinerary so all you have to do is go. What are you waiting for? Adventure awaits!</p>
         </div>
-        <button id="plan-button" onClick={this.handlePlan}>Plan Your Adventure</button>
+        <button className="submitBtn" onClick={this.handlePlan}>Plan Your Adventure Today</button>
       </div>
     );
   }
